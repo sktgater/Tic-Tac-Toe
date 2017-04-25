3fdbbd1de16cc7af47916507072c813a8663ec58
Li Wang <sktgater@gmail.com>
2017-04-25 11:02:38 -0400

    Minor Bug Fix. Finish up game setup. Next Step: Work on player2plays() AI algorithm

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -2,7 +2,7 @@
 //Date: Apr 24 2017
 //Title: Tic Tac Toe game with AI experiments
 //Description:
-//This game is for testing various AI algorithms for Tic Tac Game.
+//This game is for testing various AI algorithms in Tic Tac Toe.
 
 
 // Select Elements
@@ -15,13 +15,13 @@ var mediumbtn = $("#mediumbtn");
 var hardbtn = $("#hardbtn");
 
 // Game Variables
-var matrix = [];
-var row = [0,0,0,0];
-for (var i = 0; i < 4; i++){
-	matrix.push(row);
+var matrix = new Array(4);
+for (var i = 0; i < matrix.length; i++){
+	matrix[i] = new Array(4);
 };
+reset();
 var mode = "hard";
-
+var gameDone = false;
 
 // Easy Button
 easybtn.on("click", function(){
@@ -35,6 +35,7 @@ easybtn.on("click", function(){
 
 	// Change game to EASY setting
 	mode = "easy";
+
 });
 
 // Medium Button
@@ -48,6 +49,7 @@ mediumbtn.on("click", function(){
 	reset();
 	// Change game to MEDIUM setting
 	mode = "medium";
+
 });
 
 
@@ -62,6 +64,7 @@ hardbtn.on("click", function(){
 	reset();
 	// Change game to HARD setting
 	mode = "hard";
+
 });
 
 // Reset button
@@ -71,18 +74,38 @@ resetBtn.on("click", function () {
 });
 
 // Player 1 Clicks on squares. Change color to blue to mark player 1's move
+// Update matrix for player 1. Then player 2 plays
 squares.on("click", function(){
 	$(this).addClass("player1");
 	// Change cell value in matrix
+	var position = $(this).attr('id');
+	var row = position.charAt(1);
+	var col = position.charAt(3);
+	matrix[row][col] = 1;
+
+	// Player 2 plays
+	player2plays();
 });
 
 // Reset all squares to be empty
 function reset(){
+	// Display Reset
 	squares.removeClass("player1 player2");
 	messageDisaplay.textContent = "";
+
+	// Game Data Reset
+	gameDone = false;
+	for (var i= 0; i < matrix.length; i++){
+		for (var j = 0; j < matrix[0].length; j++){
+			matrix[i][j] = 0;
+		}
+	}
 }
 
+// Player 2 Plays
+function player2plays(){
 
+}
 
 
 
