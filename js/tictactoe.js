869e7ed7d52517dac2f1c39cd5fb425005fe8967
Li Wang <sktgater@gmail.com>
2017-04-27 03:17:05 -0400

    finish up game algorithm. Next Step: implement easy and medium mode

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -6,7 +6,7 @@
 
 // Select Elements
 var squares = $(".square");
-var messageDisaplay = $("#message");
+var messageDisaplay = document.querySelector("#message");
 var h1 = $("h1");
 var resetBtn = $("#reset");
 var easybtn = $("#easybtn");
@@ -15,12 +15,12 @@ var hardbtn = $("#hardbtn");
 
 // Game Variables
 var matrix = build();
-var gameDone;
 var move_count;		// count how many moves so far
 reset();
 var mode = "hard";
 var min_utility = -1000;
 var max_utility = 1000;
+var action;			// global variable "action" that saves the action in DFS
 
 // Easy Button
 easybtn.on("click", function(){
@@ -68,7 +68,7 @@ hardbtn.on("click", function(){
 
 // Reset button
 resetBtn.on("click", function () {
-	// reset all squares
+	// reset
 	reset();
 });
 
@@ -84,30 +84,60 @@ squares.on("click", function(){
 	matrix[row][col] = 1;
 	move_count++;
 
-	// Player 2 plays
+	switch(terminal_test(matrix)){
+		case 0:
+			messageDisaplay.textContent = "Draw Game";
+			break;
+		case 1:
+			messageDisaplay.textContent = "You Win!";
+			break;
+		case 2:
+			messageDisaplay.textContent = "You Lose!";
+			break;
+		default:
+			break;
+	}
+	// player 2 plays
 	player2plays();
+	switch(terminal_test(matrix)){
+		case 0:
+			messageDisaplay.textContent = "Draw Game";
+			break;
+		case 1:
+			messageDisaplay.textContent = "You Win!";
+			break;
+		case 2:
+			messageDisaplay.textContent = "You Lose!";
+			break;
+		default:
+			break;
+	}
 });
 
 // Player 2 Plays
 function player2plays(){
+
 	// Calculate right move
-	var move = search(matrix);
+	if (mode == "hard"){
+		search(matrix);
+	}
+	
 	// Change matrix cell
-	matrix[move[0]][move[1]] = 2;
+	matrix[action[0]][action[1]] = 2;
+	
 	// Change display
-	var id = "r" + move[0] + "c" + move[1];
+	var id = "r" + action[0] + "c" + action[1];
 	$("#" + id).addClass("player2");
+
+	move_count++;
 }
 
 // Player 2 finds its next move by looking at the current state
 function search(state){
 
-	// 
 	// assign utility variable v to max_value function call
-	var v = max_value(state, min_utility, max_utility, previous_action);
-	
-	// return the action in actions(state) with value v
-	
+	var v = max_value(state, min_utility, max_utility);
+
 }
 
 // Max search
@@ -115,12 +145,16 @@ function max_value(state, alpha, beta){
 	var test = terminal_test(state);
 	if (test >= 0){return utility[test]};
 	var v = min_utility;
-	actions(state).forEach(function(a){
-		var search_res = min_value(result(state, a, 2), alpha, beta);
-		v = Math.max(v, search_res)
+	var max_actions = actions(state);
+	for (var i = 0; i < max_actions.length; i++){
+		var search_res = min_value(result(state, max_actions[i], 2), alpha, beta);
+		if (search_res >= v){
+			action = max_actions[i];
+			v = search_res;
+		}
 		if (v >= beta){return v;}
 		alpha = Math.max(alpha, v);
-	});
+	}
 	return v;
 }
 
@@ -129,12 +163,13 @@ function min_value(state, alpha, beta){
 	var test = terminal_test(state);
 	if (test >= 0){return utility[test]};
 	var v = max_utility;
-	actions(state).forEach(function(a){
-		var search_res = max_value(result(state, a, 1), alpha, beta);
-		v = Math.min(v, search_res)
+	var min_actions = actions(state);
+	for (var i = 0; i < min_actions.length; i++){
+		var search_res = max_value(result(state, min_actions[i], 2), alpha, beta);
+		v = Math.min(v, search_res);
 		if (v <= alpha){return v;}
 		beta = Math.min(beta, v);
-	});
+	}
 	return v;
 }
 
@@ -152,38 +187,51 @@ function result(state, act, player){
 	return temp;
 }
 
-// Test if game is done
+// Test if game is done. 
+// Return 1 for player 1 wins; 2 for player 2 wins; 0 for draw; -1 for unfinished game
 function terminal_test(state){
-
+	var s = new Set();
 	// Horizontal line test
 	for (var i = 0; i < state.length; i++){
-		if (state[i][0] == state[i][1] == state[i][2] == state[i][3] != 0){
-			gameDone = true;
+		for (var j = 0; j < state[0].length; j++){
+			s.add(state[i][j]);
+		}
+		if (s.size == 1 && !s.has(0)){
 			return state[i][0];
 		}
+		s.clear();
 	}
+	s.clear();
 
 	// Vertical line test
 	for (var j = 0; j < state[0].length; j++){
-		if (state[0][j] == state[1][j] == state[2][j] == state[3][j] != 0){
-			gameDone = true;
+		for (var i = 0; i < state.length; i++){
+			s.add(state[i][j]);
+		}
+		if (s.size == 1 && !s.has(0)){
 			return state[0][j];
 		}
+		s.clear();
 	}
+	s.clear();
 
 	// Diagonal line test
-	if (state[3][0] == state[2][1] == state[1][2] == state[0][3] != 0){
-		gameDone = true;
+	for (var i = 3, j = 0; i >= 0; i--, j++){
+		s.add(state[i][j]);
+	}
+	if (s.size == 1 && !s.has(0)){
 		return state[3][0];
 	}
-	if (state[0][0] == state[1][1] == state[2][2] == state[3][3] != 0){
-		gameDone = true;
+	s.clear();
+	for (var i = 0, j = 0; i < 4; i++, j++){
+		s.add(state[i][j]);
+	}
+	if (s.size == 1 && !s.has(0)){
 		return state[0][0];
 	}
 
 	// Draw test
 	if (move_count == 16){
-		gameDone = true;
 		return 0
 	}
 	
@@ -201,6 +249,28 @@ var utility = {
 // Given current state, return a list of possible actions (matrix position)
 function actions(state){
 	var result = []
+
+	// shuffle an array. This is to randomize actions array
+	function shuffle(array) {
+  		var currentIndex = array.length, temporaryValue, randomIndex;
+
+	  	// While there remain elements to shuffle...
+	  	while (0 !== currentIndex) {
+
+	    // Pick a remaining element...
+	    randomIndex = Math.floor(Math.random() * currentIndex);
+	    currentIndex -= 1;
+
+	    // And swap it with the current element.
+	    temporaryValue = array[currentIndex];
+	    array[currentIndex] = array[randomIndex];
+	    array[randomIndex] = temporaryValue;
+	 	}
+
+  		return array;
+	}
+
+	// find all cells in matrix that has value 0. It is a possible action.
 	for (var i = 0; i < state.length; i++){
 		for (var j = 0; j < state[0].length; j++){
 			if (state[i][j] == 0){
@@ -208,6 +278,8 @@ function actions(state){
 			}
 		}
 	}
+
+	result = shuffle(result);
 	return result
 }
 
@@ -227,13 +299,13 @@ function reset(){
 	messageDisaplay.textContent = "";
 
 	// Game Data Reset
-	gameDone = false;
 	move_count = 0;
 	for (var i= 0; i < matrix.length; i++){
 		for (var j = 0; j < matrix[0].length; j++){
 			matrix[i][j] = 0;
 		}
 	}
+	action = undefined;
 }
 
 
